a0ea53cbd8c496026d9dd8735d5eb5edd213492b
Joel Chantrell <joelchantrell@gmail.com>
2022-01-10 21:39:48 +1030

    Initial version

---
+++ script.js
@@ -0,0 +1,14 @@
+function operate(operator, a, b)
+
+function add(a, b);
+
+function subtract(a, b);
+
+function multiply(a, b);
+
+function divide(a, b);
+
+function displayInput();
+
+function clearInput();
+
