33e35e2a239c192f27fa2f608046a1fdeeaae50b
Joel Chantrell <joelchantrell@gmail.com>
2022-01-14 22:00:16 +1030

    Refactored majority

--- script.js
+++ script.js
@@ -1,103 +1,100 @@
-let userInput = "";
-let storedInput = "";
-let outputValue = "";
-const storedInputText = document.querySelector('.stored-input')
-const inputRow = document.querySelector('.input-row')
-const input = document.querySelector('.input');
-const output = document.querySelector('.output');
-const operator = document.querySelector('.operator');
-const backspace = document.querySelector('.backspace');
-const clear = document.querySelector('.clear');
-const buttons = document.querySelectorAll('.button');
-buttons.forEach(buttonValue => buttonValue.addEventListener('click', event => {
-    clickButton(event.target.getAttribute("data-input"));
-}));
+const numberKey = document.querySelectorAll('[data-num]');
+const operatorKey = document.querySelectorAll('[data-op]');
+const decimalKey = document.querySelector('[data-decimal]');
+const equalsKey = document.querySelector('[data-equals]');
+const backspaceKey = document.querySelector('.backspace');
+const clearKey = document.querySelector('[data-clear]');
+const inputField = document.querySelector('.input');
+const outputField = document.querySelector('.output');
 
-function clickButton(buttonValue){
-    if (buttonValue == "clear"){
-        clearScreen("all")
-    }
-    else if (buttonValue == "backspace"){
-        removeLastLetter = userInput.slice(0, -1);
-        userInput = removeLastLetter;
-    }
-    else if (buttonValue == "+"){
-        if (storedInput != 0){
-            sum = add(parseInt(userInput), parseInt(storedInput))
-            input.textContent = "0"
-            userInput = "0"
-            storedInputText.textContent = ""
-            storedInput = "0"
-            operator.textContent = ""
-            return sum;
+let userInput = '';
+let storedInput = '';
+let output = '';
 
-        }
-        if (userInput != 0){
-            storedInput = userInput;
-            operator.textContent = buttonValue;
-            storedInputText.textContent = userInput;
-            userInput = 0;
-        }
+// set up listener on each number key, input value of key on press
+numberKey.forEach((key) => key.addEventListener('click', () => inputNumber(key.textContent)))
+
+// set up listener on each operator key, input value of key on press
+operatorKey.forEach((key) => key.addEventListener('click', () => inputOperator(key.textContent)))
+
+// set up listeners for all other keys
+decimalKey.addEventListener('click', inputDecimal)
+equalsKey.addEventListener('click', equals)
+backspaceKey.addEventListener('click', backspace)
+clearKey.addEventListener('click', clearData)
+
+function inputNumber(input){
+    if (inputField.textContent == '0'){
+        inputField.textContent = ''
     }
-    else if (buttonValue == "-"){
-        storedInput = userInput;
-        operator.textContent = buttonValue;
-        operate("-")
+    inputField.textContent += input;
+}
+
+function inputOperator(operator){
+
+
+}
+
+function inputDecimal(){
+    if (inputField.textContent == ''){
+        inputField.textContent = '0'
     }
-    else if (buttonValue == "*"){
-        storedInput = userInput;
-        operator.textContent = buttonValue;
-        operate("*")
+    else if (inputField.textContent.includes('.')){
+        return;
     }
-    else if (buttonValue == "/"){
-        storedInput = userInput;
-        operator.textContent = buttonValue;
-        operate("/")
-    }
-    else if (userInput == 0){
-        userInput = buttonValue;
-        input.textContent = userInput;
-    }
-    else
-        userInput = userInput += buttonValue;
-        input.textContent = userInput;
-};
+    else inputField.textContent += '.'
+}
 
 function operate(operator){
-
-};
-
-function add(num1, num2){
-    sum = num1 + num2;
-    output.textContent = `${userInput} + ${storedInput} = ${sum}`
-};
-
-function subtract(){
-
-};
-
-function multiply(){
-
-};
-
-function divide(){
-
-};
-
-function clearScreen(instruction){
-    if (instruction == "all"){
-        input.textContent = "0"
-        userInput = "0"
-        output.textContent = ""
-        outputValue = ""
-        operator.textContent = ""
-        storedInput = ""
-        storedInputText.textContent = ""
+    if (inputField.textContent != "0"){
+        userInput = inputField.textContent;
+        outputField.textContent = userInput;
     }
-    if (instruction == "input"){
-        input.textContent = "0"
-        userInput = "0"
+}
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+function percentage(a, b){
+    if (a == '0' && b == '0'){
+        outputField.textContent = "Not possible."
     }
-};
+    sum = (100 * a) / b;
+    return sum;
+}
 
+function equals(a, b){
 
+}
+
+function clearScreen(){
+
+}
+
+function clearData() {
+    inputField.textContent = '0';
+    outputField.textContent = '';
+    userInput = '';
+    storedInput = '';
+    output = '';
+}
+
+function backspace() {
+    inputField.textContent = inputField.textContent.slice(0, -1);
+    if (inputField.textContent == ''){
+        inputField.textContent = '0'
+    }
+}
